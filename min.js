2e58adf4613bcc19a2dc4377f84ecfa99465a968
tk500 <62159155+tk500@users.noreply.github.com>
2021-04-21 16:48:13 +0900

    Update min.js

--- min.js
+++ min.js
@@ -13,7 +13,33 @@ script.addEventListener('load', function () {
     $("#hide_bottom").click(function(){
     $("#hide").fadeToggle(1000);
     });
+  });
+
+  $(document).ready(function(){
+    $("#hide_skills").click(function(){
+    $("#skills_hide").fadeToggle(1000);
     });
+  });
+
+  $('#hide_bottom, #hide_skills').on('click',function(){
+    // クリックされたら次の要素を操作
+    $(this).next().slideToggle();
+    $(this).next().toggleClass('active');
+    $(this).toggleClass('active');
+   });
+
+  //  ヘッダーメニューの開閉
+   $(document).ready(function(){
+     $("#click_s").click(function(){
+     $("#menu").slideToggle(1000);
+     });
+   });
+  //  ヘッダーメニュー選択時に閉じる
+   $('#click, #click2, #click3').on('click',function(){
+     $("#menu").fadeOut(1000);
+    });
+    
+
 })
 
 document.head.appendChild(script)
